9eb1d06ef5fb27e80535f3a18463b6d4122c923a
oksbelysheva <oks.belysheva@yandex.ru>
2020-03-28 18:52:06 +0400

    init: initial project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+          
+      </header>
+    </div>
+  );
+};
+
+export default App;
